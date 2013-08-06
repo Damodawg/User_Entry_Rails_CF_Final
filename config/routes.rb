019aa1049f5e7e41e4bd608b39bc5d0b7fb25557
Damian Wylie <damianwylie@gmail.com>
2013-08-06 10:16:52 -0700

    Edited initial route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Usermgr::Application.routes.draw do
+  get "welcome/index"
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
